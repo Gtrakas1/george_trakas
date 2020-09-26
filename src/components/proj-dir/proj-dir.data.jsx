7b81646bc5632325c13b94c7693ac074309888a7
gtrakas1 <gtrakas23@gmail.com>
2020-09-25 23:41:05 -0600

    Added a project to WorkPage

--- src/components/proj-dir/proj-dir.data.jsx
+++ src/components/proj-dir/proj-dir.data.jsx
@@ -3,16 +3,24 @@ const ProjData = [
         id: 1,
         title: 'graphql-store-app',
         paragraph: 'This project is the the back-end to a store-app. A manager could sign into the database and add, remove or update items to each catergory to sell',
-        imageUrl: 'https://i.ibb.co/9hd3Pwz/store-app.png',
+        imageUrl: 'https://i.ibb.co/tpxCw0f/store-app.png',
         urlLink: 'https://vast-badlands-66278.herokuapp.com/'
     },
     {
         id: 2,
         title: 'monsters-rolodex',
         paragraph: 'A static-website I learned from the React course I am currently taking. It taught functions such as map(), filer() and went in depth on class/function components and state',
-        imageUrl: 'https://i.ibb.co/Mh4mD3C/monsters.png',
+        imageUrl: 'https://i.ibb.co/R0t1Qrn/monsters.png',
         urlLink: 'https://gtrakas1.github.io/React/'
     },
+    {
+        id: 2,
+        title: 'crwn-clothing',
+        paragraph: 'An ecommerce website utizing state management for certain instances, OAuth authentication with a firebase database that stores users that sign up and react-router to proceed to multiple webpages.',
+        imageUrl: 'https://i.ibb.co/FbLC7Jm/crwn-pic.png',
+        urlLink: 'https://crwn-live-trakas.herokuapp.com/'
+    }
+
 
 ]
 
