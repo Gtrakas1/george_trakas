35fddd4d9824c5005b12897fb629b99ecd7955e0
gtrakas1 <gtrakas23@gmail.com>
2020-10-01 20:34:03 -0600

    changed the icon and the name in the tab and added the url to a more modern resume

--- src/components/proj-dir/proj-dir.data.jsx
+++ src/components/proj-dir/proj-dir.data.jsx
@@ -1,22 +1,22 @@
 const ProjData = [
     {
         id: 1,
-        title: 'graphql-store-app',
-        paragraph: 'This project is the the back-end to a store-app. A manager could sign into the database and add, remove or update items to each catergory to sell',
+        title: 'GraphQL-store-app',
+        paragraph: 'This project is the back-end to a store-app. A manager can create an acouunt to sign into the database and add, remove or update items to each catergory to sell',
         imageUrl: 'https://i.ibb.co/tpxCw0f/store-app.png',
         urlLink: 'https://vast-badlands-66278.herokuapp.com/'
     },
     {
         id: 2,
-        title: 'monsters-rolodex',
-        paragraph: 'A static-website I learned from the React course I am currently taking. It taught functions such as map(), filer() and went in depth on class/function components and state',
+        title: 'Monsters-Rolodex',
+        paragraph: 'A React static-website that utilizes functions such as filter(), map() and fetch(). It also includes function and class components',
         imageUrl: 'https://i.ibb.co/R0t1Qrn/monsters.png',
         urlLink: 'https://gtrakas1.github.io/React/'
     },
     {
-        id: 2,
-        title: 'crwn-clothing',
-        paragraph: 'An ecommerce website utizing state management for certain instances, OAuth authentication with a firebase database that stores users that sign up and react-router to proceed to multiple webpages.',
+        id: 3,
+        title: 'Crwn-Clothing',
+        paragraph: 'An ecommerce website utilizing state management for certain instances, OAuth authentication with a firebase database that stores users and react-router to proceed to multiple webpages.',
         imageUrl: 'https://i.ibb.co/FbLC7Jm/crwn-pic.png',
         urlLink: 'https://crwn-live-trakas.herokuapp.com/'
     }
--- src/components/site-item/site_item.data.jsx
+++ src/components/site-item/site_item.data.jsx
@@ -22,7 +22,7 @@ const SiteIcon = [
         id: 2,
         title: 'resume',
         imageUrl: 'https://i.ibb.co/TKHm0yj/resume.png',
-        urlLink: 'https://docs.google.com/document/d/e/2PACX-1vSbGe3JE_YeMiXw-mcelCRcW-rp-4f-bBepOSs4lCZ2Ein3pC-ULcmHtop7opLlSg/pub?embedded=true'
+        urlLink: 'https://drive.google.com/file/d/1yBakbyB62TqvwEeyMsCKyUt-BpTB7eMl/view?usp=sharing'
     },
 
 
