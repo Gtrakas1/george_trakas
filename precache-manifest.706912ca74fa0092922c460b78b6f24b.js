ebad71bc5e8ce0a719cb86858af93481f882779b
gtrakas1 <gtrakas23@gmail.com>
2020-10-01 21:38:23 -0600

    Updates

---
+++ precache-manifest.706912ca74fa0092922c460b78b6f24b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04e7020a5a31cd6e7526da834298fe0a",
+    "url": "/george_trakas/index.html"
+  },
+  {
+    "revision": "6774e6dde81616374543",
+    "url": "/george_trakas/static/css/main.91355a86.chunk.css"
+  },
+  {
+    "revision": "16164e44721785226ef3",
+    "url": "/george_trakas/static/js/2.af4fada0.chunk.js"
+  },
+  {
+    "revision": "f9e7e68e90600aabdd7d1f759413461d",
+    "url": "/george_trakas/static/js/2.af4fada0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6774e6dde81616374543",
+    "url": "/george_trakas/static/js/main.2dc9effd.chunk.js"
+  },
+  {
+    "revision": "016f2164a4836ae3c368",
+    "url": "/george_trakas/static/js/runtime-main.73d87969.js"
+  },
+  {
+    "revision": "d617c985463e7218c3b6f49ecdd1d72c",
+    "url": "/george_trakas/static/media/George-Landscape2-BW.d617c985.jpg"
+  },
+  {
+    "revision": "a4e1a46b54d14f44d89fe1b825b8b6ce",
+    "url": "/george_trakas/static/media/George-Portrait1-BW-lowres.a4e1a46b.jpg"
+  }
+]);
